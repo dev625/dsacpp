2a6bba22a114e5dc6745d21c3b8cc90b30e0250c
= <witdx514@gmail.com>
2020-07-23 10:43:38 +0530

    Binary Search

---
+++ Binary_Search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int RecursiveBinarySearch(int *arr,int ele,int l,int r)
+{
+    while(l<=r)
+    {
+        int mid = (l+r)/2;
+        if(arr[mid]==ele)
+            return mid;
+        else if(arr[mid]>ele)
+            return RecursiveBinarySearch(arr,ele,l,mid-1);
+        else
+            return RecursiveBinarySearch(arr,ele,mid+1,r);
+    }
+    return -1;
+}
+
+int main()
+{
+    int i,len,ele;
+    cout << "Enter the length of array:\n";
+    cin >> len;
+    int *arr = new int[len];
+    cout << "Enter the elements of the array in sorted order: \n";
+    for(i=0;i<len;i++)
+        cin >> arr[i];
+    cout << "Which element do you want to search for ?\n";
+    cin >> ele;
+    if(RecursiveBinarySearch(arr,ele,0,len-1)==-1)
+        cout << "Element "<<ele<<" not present in the array.\n";
+    else
+        cout << "Element "<<ele <<" found at index : "<<RecursiveBinarySearch(arr,ele,0,len-1)<<"\n";
+}
