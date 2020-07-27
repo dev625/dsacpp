de70adecaf79a49b5c6bb289442b4eb7c87ed5f5
= <witdx514@gmail.com>
2020-07-27 22:34:07 +0530

    Level Order Insertion

---
+++ BT_Level_Order_M1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* left, *right;
+        node(int data)
+        {
+            this->data = data;
+            this->left = this->right = nullptr;
+        }
+};
+
+int height(node* node)
+{
+    int x,y;
+    if(node!=nullptr)
+    {
+        x = height(node->left);
+        y = height(node->right);
+        if(x>y)
+            return x+1;
+        else
+            return y+1;
+    }
+    return 0;
+}
+
+void printGivenLevel(node* root,int level)
+{
+    if(root==nullptr)
+        return;
+    if(level==1)
+        cout << root->data << " ";
+    else if(level > 1)
+    {
+        printGivenLevel(root->left,level-1);
+        printGivenLevel(root->right,level-1);
+    }
+}
+void printLevelOrder(node* root)
+{
+    int h = height(root);
+    int i;
+    for(i=1;i<=h;i++)
+        printGivenLevel(root,i);
+}
+int main()
+{
+    node * root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right =  new node(7);
+    printLevelOrder(root);
+}
---
+++ BT_Level_Order_M2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* left, *right;
+        node(int data)
+        {
+            this->data = data;
+            this->left = this->right = nullptr;
+        }
+};
+
+void printLevelOrder(node* root)
+{
+    //base case
+    if(root==nullptr)
+        return;
+    //create an empty queue for level order traversal
+    queue<node *>q;
+    q.push(root);
+    while(q.empty()==false)
+    {
+        node * node = q.front();
+        cout << node->data << " ";
+        q.pop();
+
+        if(node->left!=nullptr)
+            q.push(node->left);
+        if(node->right!=nullptr)
+            q.push(node->right);
+    }
+}
+
+
+
+
+
+int main()
+{
+    node * root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right =  new node(7);
+    printLevelOrder(root);
+}
